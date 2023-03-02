a6503bc9f1edc46cefc58ff3a2a89b462ef80259
Harsh <harshuniverse000@gmail.com>
2023-03-02 11:42:10 +0530

    finished

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./styles/index.css";
 import App from "./components/App";
+import { BrowserRouter as Router } from "react-router-dom";
+import store from "./redux/Store";
+import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>
 );
